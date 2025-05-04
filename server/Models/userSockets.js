7b69b2a3dd2767a9efc665c538e63a186e792e4a
inthrakumar <inthrak04@gmail.com>
2025-05-04 13:49:33 +0530

    removal of server fodler

--- server/Models/userSockets.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSocketSchema = new mongoose.Schema({
-    username: String,
-    socketId: String,
-    isLogged: { type: Boolean, default: false}
-});
-const userSocket=mongoose.model('userSocket',userSocketSchema);
-module.exports=userSocket;
--- server/middlewares/AdminMiddleware.js
+++
@@ -1,9 +0,0 @@
-const admin_verify=(req,res,next)=>{
-    if(req.isAdmin){
-        next();
-    }else{
-        return res.status(403).json({message:"Access denied. You are not an admin."});
-    }
-}
-
-module.exports={admin_verify};
